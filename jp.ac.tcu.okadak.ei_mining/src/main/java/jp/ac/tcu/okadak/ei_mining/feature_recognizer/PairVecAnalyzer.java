e14880763bcd151292a0297812b47c90458a6fff
Koji-Okada <koji.okada.prog@gmail.com>
2018-08-18 17:37:44 +0900

    平均の比の処理を追加

--- jp.ac.tcu.okadak.ei_mining/src/main/java/jp/ac/tcu/okadak/ei_mining/feature_recognizer/PairVecAnalyzer.java
+++ jp.ac.tcu.okadak.ei_mining/src/main/java/jp/ac/tcu/okadak/ei_mining/feature_recognizer/PairVecAnalyzer.java
@@ -6,7 +6,7 @@ import org.apache.commons.math3.distribution.FDistribution;
  * ベクトルデータの一対比較を行う.
  *
  * @author K.Okada
- * @version 2018.08.16
+ * @version 2018.08.18
  */
 public class PairVecAnalyzer {
 
@@ -119,7 +119,7 @@ public class PairVecAnalyzer {
 
 		final int minDegreeOfFreedom = 3; // 最小自由度
 
-		// 有効要素数のカウントと平均値算出処理を行う
+		// 有効要素数をカウントする
 		double sum0 = 0.0e0d;
 		double sum1 = 0.0e0d;
 		int c0 = 0;
@@ -135,6 +135,14 @@ public class PairVecAnalyzer {
 				c1++;
 			}
 		}
+
+		if ((c0 < minDegreeOfFreedom) || (c1 < minDegreeOfFreedom)) {
+			// どちらかが最小自由度未満の場合
+			this.pValue = null; // p値もリセットする
+			return null;
+		}
+
+		// 平均値を算出する
 		double avg0 = sum0 / (double) c0;
 		double avg1 = sum1 / (double) c1;
 
@@ -151,13 +159,8 @@ public class PairVecAnalyzer {
 				sumS1 += d1 * d1;
 			}
 		}
-		if ((c0 < minDegreeOfFreedom) || (c1 < minDegreeOfFreedom)) {
-			// どちらかが最小自由度未満の場合
-			this.pValue = null; // p値もリセットする
-			return null;
-		}
 
-		double aSim = (sumS0 / (double) c0) / (sumS1 / (double) c1);
+		double rVar = (sumS0 / (double) c0) / (sumS1 / (double) c1);
 
 		// 振幅比の p値を算出する
 		double dF0 = (double) (c0 - 1);
@@ -174,9 +177,103 @@ public class PairVecAnalyzer {
 			pV = 1.0e0d - fD.cumulativeProbability(variance1 / variance0);
 
 		}
-		this.pValue = pV;		// 片側検定値
+		this.pValue = pV; // 片側検定値
 
+		return (Double) rVar;
+	}
 
-		return (Double) aSim;
+	/**
+	 * 欠損値も考慮して 2つの系列データの平均値の比を算出する.
+	 *
+	 * 竹松の経営情報学会発表時には、比ではなく差で判定しているが
+	 * 指標の特質に依存する差よりも比を用いた方が良い
+	 *
+	 *
+	 * @param data0
+	 *            時系列データ(基準)
+	 * @param data1
+	 *            時系列データ(対象)
+	 * @param s
+	 *            期間開点
+	 * @param e
+	 *            期間終点
+	 * @return 平均値の比
+	 * 			∞ : 当該時系列が他に比べ小さい
+	 * 			 1 : 同一
+	 * 			 0 : 当該時系列が他に比べ大きい
+	 */
+	public final Double calcAvgRatio(final Double[] data0, final Double[] data1,
+			final int s, final int e) {
+
+		final int minDegreeOfFreedom = 3; // 最小自由度
+
+		// 有効要素数をカウントする
+		double sum0 = 0.0e0d;
+		double sum1 = 0.0e0d;
+		int c0 = 0;
+		int c1 = 0;
+		for (int i = s; i <= e; i++) {
+			if (null != data0[i]) {
+				sum0 += data0[i];
+				c0++;
+
+			}
+			if (null != data1[i]) {
+				sum1 += data1[i];
+				c1++;
+			}
+		}
+
+		if ((c0 < minDegreeOfFreedom) || (c1 < minDegreeOfFreedom)) {
+			// どちらかが最小自由度未満の場合
+			this.pValue = null; // p値もリセットする
+			return null;
+		}
+
+		// 系列毎の平均値を算出する
+		double avg0 = sum0 / (double) c0;
+		double avg1 = sum1 / (double) c1;
+
+		// 総平均値を算出する
+		double avg = (avg0 * (double) c0 + avg1 * (double) c1) / (double) (c0
+				+ c1);
+
+		// 偏差平方和の算出
+		double vSSTR = (avg0 - avg) * (avg0 - avg) * c0 + (avg1 - avg) * (avg1
+				- avg) * c1;
+
+		// 系列ごとの偏差平方和を算出する
+		double sumS0 = 0.0e0d;
+		double sumS1 = 0.0e0d;
+		for (int i = s; i <= e; i++) {
+			if (null != data0[i]) {
+				double d0 = data0[i] - avg0;
+				sumS0 += d0 * d0;
+			}
+			if (null != data1[i]) {
+				double d1 = data1[i] - avg1;
+				sumS1 += d1 * d1;
+			}
+		}
+		double vSSE = sumS0 + sumS1;
+
+		// 自由度の算出
+		double dFTR = 1.0e0; // 2群比較なので常に 2-1=1
+		double dFE = (double) (c0 + c1 - 2);
+
+		// 標準偏差の算出
+		double vMSTR = vSSTR / dFTR;
+		double vMSE = vSSE / dFE;
+
+		// 対象時系列データから見た基準時系列データの水準の差
+		double diff = avg0 / avg1;
+
+		// 平均値の差の p値を算出する
+		double pV;
+		FDistribution fD = new FDistribution(dFTR, dFE);
+		pV = 1.0e0d - fD.cumulativeProbability(vMSTR / vMSE);
+		this.pValue = pV; // 片側検定値
+
+		return (Double) diff;
 	}
 }
--- jp.ac.tcu.okadak.ei_mining/src/test/java/jp/ac/tcu/okadak/ei_mining/feature_recognizer/PairVecAnalyzerTest.java
+++ jp.ac.tcu.okadak.ei_mining/src/test/java/jp/ac/tcu/okadak/ei_mining/feature_recognizer/PairVecAnalyzerTest.java
@@ -8,13 +8,13 @@ import org.junit.Test;
 /**
  *
  * @author K.Okada
- * @version 2018.08.16
+ * @version 2018.08.18
  */
 public class PairVecAnalyzerTest {
 
 	@Test
 	public void testCosSimularity() {
-		final double DELTA = 1e-4;	// Rの表示精度に合わせる
+		final double DELTA = 1e-3;	// Rの表示精度に合わせる
 
 		PairVecAnalyzer pva = new PairVecAnalyzer();
 		Double cor;
@@ -46,7 +46,7 @@ public class PairVecAnalyzerTest {
 		// R上で
 		// data3 <- c(1.0, 2.0, 4.0, 5.0)
 		// data4 <- c(9.0, 6.0, 7.0, 5.0)
-		// cor.test(data1, data2)
+		// cor.test(data3, data4)
 		assertEquals(cor, -0.7483315e0d, DELTA);
 		assertEquals(pValue, 0.2517e0d, DELTA);
 
@@ -59,7 +59,7 @@ public class PairVecAnalyzerTest {
 		// R上で
 		// data5 <- c(1.0, 3.0, 4.0, 5.0)
 		// data6 <- c(9.0, 8.0, 7.0, 5.0)
-		// cor.test(data1, data2)
+		// cor.test(data5, data6)
 		assertEquals(cor, -0.94285710d, DELTA);
 		assertEquals(pValue, 0.05714e0d, DELTA);
 
@@ -77,33 +77,109 @@ public class PairVecAnalyzerTest {
 
 	@Test
 	public void testAmpRatio() {
-		final double DELTA = 1e-4;	// Rの表示精度に合わせる
+		final double DELTA = 1e-3;	// Rの表示精度に合わせる
 
 		PairVecAnalyzer pva = new PairVecAnalyzer();
-		Double amp;
+		Double rVar;
 		Double pValue;
 
 		// ====
 		Double[] data1 = {1.0e0d, 3.0e0d, 2.0e0d, 4.0e0d, 5.0e0d};
 		Double[] data2 = {9.0e0d, 8.0e0d, 6.0e0d, 7.0e0d, 0.0e0d};
 
-		amp = pva.calcAmpRatio(data1, data2, 0, 4);
+		rVar = pva.calcAmpRatio(data1, data2, 0, 4);
 		pValue = pva.getPValue();
 
-//		System.out.println(amp + "\t" + pValue);
+//		System.out.println(rVar + "\t" + pValue);
 		// R上で
 		// data1 <- c(1.0, 3.0, 2.0, 4.0, 5.0)
 		// data2 <- c(9.0, 8.0, 6.0, 7.0, 0.0)
 		// var.test(data1, data2)	// 両側検定値
-		assertEquals(amp, 0.2e0d, DELTA);
+		assertEquals(rVar, 0.2e0d, DELTA);
 		assertEquals(pValue, 0.1481e0d / 2.0e0d, DELTA);
 
-		amp = pva.calcAmpRatio(data2, data1, 0, 4);
+		rVar = pva.calcAmpRatio(data2, data1, 0, 4);
 		pValue = pva.getPValue();
 
 		// 基準の入替え
-//		System.out.println(amp + "\t" + pValue);
-		assertEquals(amp, 5.0e0d, DELTA);
+//		System.out.println(rVar + "\t" + pValue);
+		assertEquals(rVar, 5.0e0d, DELTA);
 		assertEquals(pValue, 0.1481e0d / 2.0e0d, DELTA);
+
+		// ==== 欠損値のある場合
+		Double[] data3 = {1.0e0d, null, 2.0e0d, 4.0e0d, 5.0e0d};
+		Double[] data4 = {9.0e0d, 8.0e0d, null, 7.0e0d, 0.0e0d};
+		rVar = pva.calcAmpRatio(data3, data4, 0, 4);
+		pValue = pva.getPValue();
+//		System.out.println(rVar + "\t" + pValue);
+
+		// R上で
+		// data3 <- c(1.0, 2.0, 4.0, 5.0)
+		// data4 <- c(9.0, 8.0, 7.0, 0.0)
+		// var.test(data3, data4)
+		assertEquals(rVar,0.2e0d, DELTA);
+		assertEquals(pValue, 0.2191e0d / 2.0e0d, DELTA);
+
+		// ==== 最小自由度以下の場合
+		Double[] data5 = {null, null, 2.0e0d, 4.0e0d, null};
+		Double[] data6 = {9.0e0d, 8.0e0d, null, 7.0e0d, 5.0e0d};
+		rVar = pva.calcAmpRatio(data5, data6, 0, 4);
+		pValue = pva.getPValue();
+//		System.out.println(rVar + "\t" + pValue);
+
+		assertThat(rVar, is(nullValue()));
+		assertThat(pValue, is(nullValue()));
+	}
+
+	@Test
+	public void testAvgRatio() {
+		final double DELTA = 1e-3;	// Rの表示精度に合わせる
+
+		PairVecAnalyzer pva = new PairVecAnalyzer();
+		Double rAvg;
+		Double pValue;
+
+		// ====
+		Double[] data1 = {1.0e0d, 3.0e0d, 2.0e0d, 4.0e0d, 5.0e0d};
+		Double[] data2 = {9.0e0d, 8.0e0d, 6.0e0d, 7.0e0d, 5.0e0d};
+
+		rAvg = pva.calcAvgRatio(data1, data2, 0, 4);
+		pValue = pva.getPValue();
+		System.out.println(rAvg + "\t" + pValue);
+
+		// R上で
+		// data1 <- c(1.0, 3.0, 2.0, 4.0, 5.0)
+		// data2 <- c(9.0, 8.0, 6.0, 7.0, 5.0)
+		// mean(data1)/mean(data2)
+		// t.test(data1, data2)
+		assertEquals(rAvg, 0.42857142857142855e0d, DELTA);
+		assertEquals(pValue, 0.003949772803445328e0d, DELTA);
+
+		// ==== 欠損値のある場合
+		Double[] data3 = {1.0e0d, null, 2.0e0d, 4.0e0d, 5.0e0d};
+		Double[] data4 = {9.0e0d, 8.0e0d, null, 7.0e0d, 5.0e0d};
+
+		rAvg = pva.calcAvgRatio(data3, data4, 0, 4);
+		pValue = pva.getPValue();
+		System.out.println(rAvg + "\t" + pValue);
+
+		// R上で
+		// data3 <- c(1.0, 2.0, 4.0, 5.0)
+		// data4 <- c(9.0, 8.0, 7.0, 5.0)
+		// mean(data3)/mean(data4)
+		// t.test(data3, data4)		// 分散分析は t検定と等価
+		assertEquals(rAvg, 0.4137931e0d, DELTA);
+		assertEquals(pValue, 0.0146e0d, DELTA);
+
+		// ==== 最小自由度以下の場合
+		Double[] data5 = {null, null, 2.0e0d, 4.0e0d, null};
+		Double[] data6 = {9.0e0d, 8.0e0d, null, 7.0e0d, 5.0e0d};
+		rAvg = pva.calcAvgRatio(data5, data6, 0, 4);
+		pValue = pva.getPValue();
+		System.out.println(rAvg + "\t" + pValue);
+
+		assertThat(rAvg, is(nullValue()));
+		assertThat(pValue, is(nullValue()));
+
 	}
 }
