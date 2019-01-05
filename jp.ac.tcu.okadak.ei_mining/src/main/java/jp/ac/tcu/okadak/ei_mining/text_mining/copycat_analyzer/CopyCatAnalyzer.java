00d2ac80fcfad70498c4cabbcb52f509d4ec2331
Koji-Okada <koji.okada.prog@gmail.com>
2019-01-05 18:29:30 +0900

    模倣戦略分析器の試作開始

---
+++ jp.ac.tcu.okadak.ei_mining/src/main/java/jp/ac/tcu/okadak/ei_mining/text_mining/copycat_analyzer/CopyCatAnalyzer.java
@@ -0,0 +1,28 @@
+package jp.ac.tcu.okadak.ei_mining.text_mining.copycat_analyzer;
+
+/**
+ * 模倣戦略分析器.
+ *
+ * @author K.Okada
+ * @version 2019.01.05
+ */
+public class CopyCatAnalyzer {
+
+	/**
+	 * 戦略ワードを抽出する.
+	 *
+	 * @param args
+	 *            ダミー引数
+	 */
+	public static void main(final String[] args) {
+
+		System.out.println("Start analyzing copycats ...");
+
+
+		System.out.println("... Fin");
+		return;
+	}
+
+
+
+}
